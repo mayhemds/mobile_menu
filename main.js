7ee91751b302b2234ee4f652ff6ebe375fdf6e81
Tyrone May <tyrone.may@live.co.uk>
2019-09-04 13:21:05 +0100

    fixed padding on section

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 
 subMenu = () => {
-    document.getElementById("myDropdown").classList.toggle("show");
+    document.getElementById("subDropdown").classList.toggle("show");
   }
   
   // Close the dropdown if the user clicks outside of it
   window.onclick = (e) => {
     if (!e.target.matches('.dropbtn')) {
-    let myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains('show')) {
-        myDropdown.classList.remove('show');
+    let subDropdown = document.getElementById("subDropdown");
+      if (subDropdown.classList.contains('show')) {
+        subDropdown.classList.remove('show');
       }
     }
   }
